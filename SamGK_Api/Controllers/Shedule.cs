e931363eae82edd926a3d4db4dea02d9d4ce09eb
kulagin <alex_kulagin@samgk.ru>
2023-11-19 14:00:40 +0400

    Shedule add

---
+++ SamGK_Api/Controllers/Shedule.cs
@@ -0,0 +1,23 @@
+using RestSharp;
+using SamGK_Api.Interfaces.Client;
+using SamGK_Api.Interfaces.Schedule;
+
+namespace SamGK_Api.Controllers;
+
+public class Shedule : _BaseController, ISсhedule
+{
+    private IEnumerable<IScheduleDate>? Get<TAccountResult>(DateOnly date, TAccountResult entity)
+    {
+        
+        var options = new RestRequest("https://mfc.samgk.ru/api/groups", Method.Get);
+        options.AddHeader("origin", "https://samgk.ru");
+        options.AddHeader("referer", "https://samgk.ru");
+
+        return null;
+    }
+    
+    IEnumerable<IScheduleDate>? ISсhedule.Get<T>(DateOnly date, T entity)
+    {
+        return Get(date, entity);
+    }
+}
--- SamGK_Api/Interfaces/Client/ISсhedule.cs
+++ SamGK_Api/Interfaces/Client/ISсhedule.cs
@@ -1,6 +1,8 @@
+using SamGK_Api.Interfaces.Schedule;
+
 namespace SamGK_Api.Interfaces.Client;
 
 public interface ISсhedule
 {
-    
+    IEnumerable<IScheduleDate>? Get<T>(DateOnly date, T entity);
 }
