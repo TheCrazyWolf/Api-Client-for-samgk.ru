ec11913e3feea6f5543aa88ee9931ec9e9ed21eb
kulagin <alex_kulagin@samgk.ru>
2023-11-19 15:16:25 +0400

    Implement multi dates

--- SamGK_Api/Controllers/ScheduleController.cs
+++ SamGK_Api/Controllers/ScheduleController.cs
@@ -28,6 +28,37 @@ public class ScheduleController : _BaseController, ISсheduleController
         return obj is null ? null : new List<IScheduleDate> { obj };
     }
 
+    public IEnumerable<IScheduleDate>? GetSchedule(DateOnly startDate, DateOnly endDate, IEmployee entity,
+        int delay = 700)
+    {
+        var sсheduleList = new List<IScheduleDate>();
+        endDate = endDate.AddDays(1);
+        while (startDate != endDate)
+        {
+            var options =
+                new RestRequest(
+                    $"https://asu.samgk.ru/api/schedule/teacher/{startDate.ToString("yyyy-MM-dd")}/{entity.Id}",
+                    Method.Get);
+            options.AddHeaders(GetHeaders());
+
+            var result = _client.Execute(options);
+
+            if (!result.IsSuccessStatusCode || result.Content is null)
+                return null;
+
+            var currentScheduleDate = JsonConvert.DeserializeObject<ScheduleDate>(result.Content);
+
+            if (currentScheduleDate?.Lessons.Count() != 0 && currentScheduleDate != null)
+                sсheduleList.Add(currentScheduleDate);
+
+            startDate = startDate.AddDays(1);
+
+            Task.Delay(delay);
+        }
+
+        return sсheduleList;
+    }
+
     public IEnumerable<IScheduleDate>? GetSchedule(DateOnly date, IGroup entity)
     {
         var options =
@@ -45,10 +76,40 @@ public class ScheduleController : _BaseController, ISсheduleController
         return obj is null ? null : new List<IScheduleDate> { obj };
     }
 
+    public IEnumerable<IScheduleDate>? GetSchedule(DateOnly startDate, DateOnly endDate, IGroup entity, int delay = 700)
+    {
+        var sсheduleList = new List<IScheduleDate>();
+        endDate = endDate.AddDays(1);
+        while (startDate != endDate)
+        {
+            var options =
+                new RestRequest($"https://asu.samgk.ru/api/schedule/{entity.Id}/{startDate.ToString("yyyy-MM-dd")}",
+                    Method.Get);
+            options.AddHeaders(GetHeaders());
+
+            var result = _client.Execute(options);
+
+            if (!result.IsSuccessStatusCode || result.Content is null)
+                return null;
+
+            var currentScheduleDate = JsonConvert.DeserializeObject<ScheduleDate>(result.Content);
+
+            if (currentScheduleDate?.Lessons.Count() != 0 && currentScheduleDate != null)
+                sсheduleList.Add(currentScheduleDate);
+
+            startDate = startDate.AddDays(1);
+
+            Task.Delay(delay);
+        }
+
+        return sсheduleList;
+    }
+
     public IEnumerable<IScheduleDate>? GetSchedule(DateOnly date, ICab entity)
     {
         var options =
-            new RestRequest($"https://asu.samgk.ru/api/schedule/cabs/{date.ToString("yyyy-MM-dd")}/cabNum/{entity.name.Replace(@"/", "_")}",
+            new RestRequest(
+                $"https://asu.samgk.ru/api/schedule/cabs/{date.ToString("yyyy-MM-dd")}/cabNum/{entity.name.Replace(@"/", "_")}",
                 Method.Get);
         options.AddHeaders(GetHeaders());
 
@@ -60,5 +121,35 @@ public class ScheduleController : _BaseController, ISсheduleController
         var obj = JsonConvert.DeserializeObject<ScheduleDate>(result.Content);
 
         return obj is null ? null : new List<IScheduleDate> { obj };
+    }
+
+    public IEnumerable<IScheduleDate>? GetSchedule(DateOnly startDate, DateOnly endDate, ICab entity, int delay = 700)
+    {
+        var sсheduleList = new List<IScheduleDate>();
+        endDate = endDate.AddDays(1);
+        while (startDate != endDate)
+        {
+            var options =
+                new RestRequest(
+                    $"https://asu.samgk.ru/api/schedule/cabs/{startDate.ToString("yyyy-MM-dd")}/cabNum/{entity.name.Replace(@"/", "_")}",
+                    Method.Get);
+            options.AddHeaders(GetHeaders());
+
+            var result = _client.Execute(options);
+
+            if (!result.IsSuccessStatusCode || result.Content is null)
+                return null;
+
+            var currentScheduleDate = JsonConvert.DeserializeObject<ScheduleDate>(result.Content);
+
+            if (currentScheduleDate?.Lessons.Count() != 0 && currentScheduleDate != null)
+                sсheduleList.Add(currentScheduleDate);
+
+            startDate = startDate.AddDays(1);
+
+            Task.Delay(delay);
+        }
+
+        return sсheduleList;
     }
 }
--- SamGK_Api/Interfaces/Client/ISсheduleController.cs
+++ SamGK_Api/Interfaces/Client/ISсheduleController.cs
@@ -8,7 +8,10 @@ namespace SamGK_Api.Interfaces.Client;
 public interface ISсheduleController
 {
     IEnumerable<IScheduleDate>? GetSchedule(DateOnly date, IEmployee entity);
+    IEnumerable<IScheduleDate>? GetSchedule(DateOnly startDate, DateOnly endDate, IEmployee entity, int delay = 700);
     IEnumerable<IScheduleDate>? GetSchedule(DateOnly date, IGroup entity);
+    IEnumerable<IScheduleDate>? GetSchedule(DateOnly startDate, DateOnly endDate, IGroup entity, int delay = 700);
     IEnumerable<IScheduleDate>? GetSchedule(DateOnly date, ICab entity);
+    IEnumerable<IScheduleDate>? GetSchedule(DateOnly startDate, DateOnly endDate, ICab entity, int delay = 700);
 
 }
