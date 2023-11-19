fe43eeff59c9d934282f3c4a13fabf7d87f2eecc
kulagin <alex_kulagin@samgk.ru>
2023-11-19 15:48:06 +0400

    DateOnly

--- SamGK_Api/Interfaces/Schedule/IScheduleDate.cs
+++ SamGK_Api/Interfaces/Schedule/IScheduleDate.cs
@@ -3,5 +3,6 @@ namespace SamGK_Api.Interfaces.Schedule;
 public interface IScheduleDate
 {
     public string Date { get; set; }
+    public DateOnly DateStructure { get; }
     public IEnumerable<ILesson> Lessons { get; set; }
 }
