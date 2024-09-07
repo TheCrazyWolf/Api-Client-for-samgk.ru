c420f2d9640bb4900de499a04a2372ff99a2a62e
TheCraztWolf <tcrazywolf63@gmail.com>
2024-09-07 12:55:54 +0400

    Удалены старые модели, не используемые интерфейсы

--- ClientSamgk/Interfaces/Client/ICabController.cs
+++ ClientSamgk/Interfaces/Client/ICabController.cs
@@ -4,20 +4,6 @@ namespace ClientSamgk.Interfaces.Client;
 
 public interface ICabController
 {
-    /// <summary>
-    /// Возращает список кабинетов
-    /// При первой загрузке список помещается в статичный лист,
-    /// при следующем обращении подтягиваются данные из листа
-    /// </summary>
-    /// <param name="forceLoad">Принудительная загрузка.
-    /// Передайте true, если хотите заново загрузить список</param>
-    /// <returns>Возращает IEnumerable&lt;ICab&gt; если все хорошо, если проблемы - null</returns>
     IList<IResultOutCab> GetCabs();
-
-    /// <summary>
-    /// Получение группы по номеру кабинета
-    /// </summary>
-    /// <param name="cabName">Номер кабинета</param>
-    /// <returns>Возращает ICab если все хорошо, если проблемы - null</returns>
     IResultOutCab? GetCab(string cabName);
 }
--- ClientSamgk/Models/Cabs/Cab.cs
+++
@@ -1,9 +0,0 @@
-using SamGK_Api.Interfaces.Cabs;
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-
-namespace SamGK_Api.Models.Cabs;
-
-public class Cab : ICab
-{
-    public string Name { get; set; }
-}
