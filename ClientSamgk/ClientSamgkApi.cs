7dd98afc0650d0a69166366392166bf127105d55
TheCraztWolf <tcrazywolf63@gmail.com>
2024-09-14 17:19:53 +0400

    Отпечатки + реализация интерфейса

--- ClientSamgk/ClientSamgkApi.cs
+++ ClientSamgk/ClientSamgkApi.cs
@@ -3,9 +3,9 @@ using ClientSamgk.Interfaces.Client;
 
 namespace ClientSamgk;
 
-public class ClientSamgkApi
+public class ClientSamgkApi : IMainClient
 {
-    public ISсheduleController Sсhedule { get; protected set; } = new ScheduleController();
+    public ISсheduleController Schedule { get; protected set; } = new ScheduleController();
     public IIdentityController Accounts { get; protected set; } = new AccountController();
     public IGroupController Groups { get; protected set; } = new GroupsController();
     public ICabController Cabs { get; protected set; } = new CabsController();
