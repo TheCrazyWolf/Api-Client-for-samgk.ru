3f2df085253555ed06fce8a6313b445547112567
TheCraztWolf <tcrazywolf63@gmail.com>
2024-09-14 17:11:29 +0400

    Исправлена опечатка

--- ClientSamgk/Interfaces/Client/IMainClient.cs
+++ ClientSamgk/Interfaces/Client/IMainClient.cs
@@ -2,7 +2,7 @@ namespace ClientSamgk.Interfaces.Client;
 
 public interface IMainClient
 {
-    public ISсheduleController Sсhedule { get; }
+    public ISсheduleController Schedule { get; }
     public IGroupController Groups { get; }
     public ICabController Cabs { get; }
 }
