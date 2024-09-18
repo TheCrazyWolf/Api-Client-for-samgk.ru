09ae6d1a55ccdcfd5f5debfb8ce52cea4bcfa038
TheCraztWolf <tcrazywolf63@gmail.com>
2024-09-18 22:34:22 +0400

    Совместимые типы

---
+++ ClientSamgkOutputResponse/LegacyImplementation/DateOnlyLegacy.cs
@@ -0,0 +1,28 @@
+﻿namespace ClientSamgkOutputResponse.LegacyImplementation;
+
+public class DateOnlyLegacy
+{
+    public int Year { get; set; }
+    public int Month { get; set; }
+    public int Day { get; set; }
+    public DayOfWeek DayOfWeek { get; set; }
+    public DateOnlyLegacy(int year, int month, int day)
+    {
+        Year = year;
+        Month = month;
+        Day = day;
+    }
+    
+    public DateOnlyLegacy(int year, int month, int day, DayOfWeek dayOfWeek)
+    {
+        Year = year;
+        Month = month;
+        Day = day;
+        DayOfWeek = dayOfWeek;
+    }
+
+    public DateOnlyLegacy()
+    {
+        
+    }
+}
---
+++ ClientSamgkOutputResponse/LegacyImplementation/TimeOnlyLegacy.cs
@@ -0,0 +1,19 @@
+﻿namespace ClientSamgkOutputResponse.LegacyImplementation;
+
+// LEGACY COMPABILITY
+public class TimeOnlyLegacy
+{
+    public int Hour { get; set; }
+    public int Minute { get; set; }
+
+    public TimeOnlyLegacy(int hour, int minute)
+    {
+        Hour = hour;
+        Minute = minute;
+    }
+
+    public TimeOnlyLegacy()
+    {
+        
+    }
+}
