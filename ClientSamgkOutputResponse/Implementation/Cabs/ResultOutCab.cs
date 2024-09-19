f80f23747bb606cf1b8c80aedbd8e16f1203425e
TheCraztWolf <tcrazywolf63@gmail.com>
2024-09-19 12:26:12 +0400

    Добавил № корпуса и отдельно кабинет

--- ClientSamgkOutputResponse/Implementation/Cabs/ResultOutCab.cs
+++ ClientSamgkOutputResponse/Implementation/Cabs/ResultOutCab.cs
@@ -5,4 +5,6 @@ namespace ClientSamgkOutputResponse.Implementation.Cabs;
 public class ResultOutCab  : IResultOutCab
 {
     public string Adress { get; set; } = string.Empty;
+    public int Campus { get; set; }
+    public string Auditory { get; set; } = string.Empty;
 }
--- ClientSamgkOutputResponse/Interfaces/Cabs/IResultOutCab.cs
+++ ClientSamgkOutputResponse/Interfaces/Cabs/IResultOutCab.cs
@@ -3,7 +3,17 @@ namespace ClientSamgkOutputResponse.Interfaces.Cabs;
 public interface IResultOutCab
 {
     /// <summary>
-    /// Кабинет
+    /// Полный адрес кабинета
     /// </summary>
     public string Adress { get; set; }
+    
+    /// <summary>
+    /// № Учебного корпуса
+    /// </summary>
+    public int Campus { get; set; }
+
+    /// <summary>
+    /// Номер кабинета
+    /// </summary>
+    public string Auditory { get; set; }
 }
