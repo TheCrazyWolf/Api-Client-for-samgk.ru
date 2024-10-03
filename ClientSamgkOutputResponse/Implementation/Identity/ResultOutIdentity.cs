2fefe3fab7050876da789226389702560953f569
TheCraztWolf <tcrazywolf63@gmail.com>
2024-10-03 21:30:58 +0400

    Переделал получение короткого имени в поле

--- ClientSamgkOutputResponse/Implementation/Identity/ResultOutIdentity.cs
+++ ClientSamgkOutputResponse/Implementation/Identity/ResultOutIdentity.cs
@@ -6,8 +6,9 @@ public class ResultOutIdentity : IResultOutIdentity
 {
     public long Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    public string ShortName => GetShortName();
 
-    public string GetShortName()
+    private string GetShortName()
     {
         var arraysTeacherName = Name.Replace("  ", string.Empty).Replace("  ", string.Empty).Split(' ');
         return arraysTeacherName.Length == 3 ? $"{arraysTeacherName[0]} {arraysTeacherName[1].FirstOrDefault()}. {arraysTeacherName[2].FirstOrDefault()}." : Name;
