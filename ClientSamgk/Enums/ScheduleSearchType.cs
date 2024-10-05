6d5850e5f19f4fc003de9df3b7e108d59c2f2eed
TheCraztWolf <tcrazywolf63@gmail.com>
2024-10-05 19:47:43 +0400

    Убраны анотации

--- ClientSamgk/Enums/ScheduleSearchType.cs
+++ ClientSamgk/Enums/ScheduleSearchType.cs
@@ -1,13 +1,8 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace ClientSamgk.Enums;
 
 public enum ScheduleSearchType
 {
-    [Display(Name = "Преподаватель")]
     Employee, 
-    [Display(Name = "Группа")]
     Group, 
-    [Display(Name = "Кабинет")]
     Cab
 }
