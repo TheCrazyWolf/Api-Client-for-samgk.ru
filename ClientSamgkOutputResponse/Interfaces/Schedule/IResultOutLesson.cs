d119f6143ee46ea3b50c24a4410947f09a566bec
TheCraztWolf <tcrazywolf63@gmail.com>
2024-12-05 22:29:44 +0400

    Атрибут о ближайшем удалении

--- ClientSamgkOutputResponse/Interfaces/Schedule/IResultOutLesson.cs
+++ ClientSamgkOutputResponse/Interfaces/Schedule/IResultOutLesson.cs
@@ -39,9 +39,11 @@ public interface IResultOutLesson
     /// <summary>
     /// Дата и время начала урока
     /// </summary>
+    [Obsolete("Свойство устарело и будет удалено в ближайшем обновлении. Используйте свойство Durations")]
     public TimeOnly DurationStart { get; set; } 
     /// <summary>
     /// Дата и время конец урока
     /// </summary>
+    [Obsolete("Свойство устарело и будет удалено в ближайшем обновлении. Используйте свойство Durations")]
     public TimeOnly DurationEnd { get; set; } 
 }
