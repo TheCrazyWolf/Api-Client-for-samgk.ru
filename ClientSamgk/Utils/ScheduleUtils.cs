ed5b2acdca841859ad73358725c5bc6c1ac9278a
TheCraztWolf <tcrazywolf63@gmail.com>
2024-12-07 22:04:17 +0400

    Добавлено новое расписание звонков

--- ClientSamgk/Utils/ScheduleUtils.cs
+++ ClientSamgk/Utils/ScheduleUtils.cs
@@ -1,12 +1,20 @@
 ﻿using ClientSamgkApiModelResponse.Shedules;
+using ClientSamgkOutputResponse.Implementation.Schedule;
 
 namespace ClientSamgk.Utils;
+
+public enum ScheduleCallType
+{
+    Standart,
+    StandartWithShift
+}
 
 public static class ScheduleUtils
 {
     public static TimeOnly GetStartLessonTime(this ScheduleItem scheduleItem, DateOnly date)
     {
-        if (date.DayOfWeek == DayOfWeek.Monday || date.DayOfWeek == DayOfWeek.Thursday && date.Month != 6 && date.Month != 7)
+        if (date.DayOfWeek == DayOfWeek.Monday ||
+            date.DayOfWeek == DayOfWeek.Thursday && date.Month != 6 && date.Month != 7)
         {
             return scheduleItem.Pair switch
             {
@@ -63,7 +71,8 @@ public static class ScheduleUtils
 
     public static TimeOnly GetEndLessonTime(this ScheduleItem scheduleItem, DateOnly date)
     {
-        if (date.DayOfWeek == DayOfWeek.Monday || date.DayOfWeek == DayOfWeek.Thursday && date.Month != 06 && date.Month != 07)
+        if (date.DayOfWeek == DayOfWeek.Monday ||
+            date.DayOfWeek == DayOfWeek.Thursday && date.Month != 06 && date.Month != 07)
         {
             return scheduleItem.Pair switch
             {
@@ -116,5 +125,112 @@ public static class ScheduleUtils
             7 when scheduleItem.Number == 2 => new TimeOnly(20, 50),
             _ => TimeOnly.MaxValue
         };
+    }
+
+    public static IList<DurationLessonDetails> GetDurationLessonDetails(this ScheduleItem scheduleItem,
+        ScheduleCallType callsType = ScheduleCallType.Standart)
+    {
+        return callsType switch
+        {
+            ScheduleCallType.Standart => GetDurationLessonDetailsStandart(scheduleItem),
+            ScheduleCallType.StandartWithShift => GetDurationLessonDetailsStandartWithShift(scheduleItem),
+            _ => throw new ArgumentOutOfRangeException(nameof(callsType), callsType, null)
+        };
+    }
+
+    private static IList<DurationLessonDetails> GetDurationLessonDetailsStandart(ScheduleItem scheduleItem)
+    {
+        List<DurationLessonDetails> scheduleCalls = scheduleItem.Pair switch
+        {
+            1 =>
+            [
+                new DurationLessonDetails(TimeOnly.Parse("08:25"), TimeOnly.Parse("09:10")),
+                new DurationLessonDetails(TimeOnly.Parse("09:15"), TimeOnly.Parse("10:00"))
+            ],
+            2 =>
+            [
+                new DurationLessonDetails(TimeOnly.Parse("10:10"), TimeOnly.Parse("10:55")),
+                new DurationLessonDetails(TimeOnly.Parse("11:00"), TimeOnly.Parse("11:45"))
+            ],
+            3 =>
+            [
+                new DurationLessonDetails(TimeOnly.Parse("12:15"), TimeOnly.Parse("13:00")),
+                new DurationLessonDetails(TimeOnly.Parse("13:05"), TimeOnly.Parse("13:50"))
+            ],
+            4 =>
+            [
+                new DurationLessonDetails(TimeOnly.Parse("14:00"), TimeOnly.Parse("14:45")),
+                new DurationLessonDetails(TimeOnly.Parse("14:50"), TimeOnly.Parse("15:35"))
+            ],
+            5 =>
+            [
+                new DurationLessonDetails(TimeOnly.Parse("15:45"), TimeOnly.Parse("16:30")),
+                new DurationLessonDetails(TimeOnly.Parse("16:35"), TimeOnly.Parse("17:20"))
+            ],
+            6 =>
+            [
+                new DurationLessonDetails(TimeOnly.Parse("17:30"), TimeOnly.Parse("18:15")),
+                new DurationLessonDetails(TimeOnly.Parse("18:20"), TimeOnly.Parse("19:05"))
+            ],
+            7 =>
+            [
+                new DurationLessonDetails(TimeOnly.Parse("19:15"), TimeOnly.Parse("20:20")),
+                new DurationLessonDetails(TimeOnly.Parse("20:05"), TimeOnly.Parse("20:50"))
+            ],
+
+            _ => []
+        };
+
+        var startIndex = scheduleItem.Number > 0 ? scheduleItem.Number - 1 : 0;
+        var endIndex = scheduleItem.Number > 0 ? startIndex : scheduleCalls.Count - 1;
+        return scheduleCalls[startIndex..endIndex];
+    }
+
+    private static IList<DurationLessonDetails> GetDurationLessonDetailsStandartWithShift(ScheduleItem scheduleItem)
+    {
+        List<DurationLessonDetails> scheduleCalls = scheduleItem.Pair switch
+        {
+            1 =>
+            [
+                new DurationLessonDetails(TimeOnly.Parse("09:15"), TimeOnly.Parse("10:00")),
+                new DurationLessonDetails(TimeOnly.Parse("10:10"), TimeOnly.Parse("10:55"))
+            ],
+            2 =>
+            [
+                new DurationLessonDetails(TimeOnly.Parse("11:00"), TimeOnly.Parse("11:45")),
+                new DurationLessonDetails(TimeOnly.Parse("12:15"), TimeOnly.Parse("13:00"))
+            ],
+            3 =>
+            [
+                new DurationLessonDetails(TimeOnly.Parse("13:05"), TimeOnly.Parse("13:50")),
+                new DurationLessonDetails(TimeOnly.Parse("14:00"), TimeOnly.Parse("14:45"))
+            ],
+            4 =>
+            [
+                new DurationLessonDetails(TimeOnly.Parse("14:50"), TimeOnly.Parse("15:35")),
+                new DurationLessonDetails(TimeOnly.Parse("15:45"), TimeOnly.Parse("16:30"))
+            ],
+            5 =>
+            [
+                new DurationLessonDetails(TimeOnly.Parse("16:35"), TimeOnly.Parse("17:20")),
+                new DurationLessonDetails(TimeOnly.Parse("17:30"), TimeOnly.Parse("18:15"))
+            ],
+            6 =>
+            [
+                new DurationLessonDetails(TimeOnly.Parse("18:20"), TimeOnly.Parse("19:05")),
+                new DurationLessonDetails(TimeOnly.Parse("19:15"), TimeOnly.Parse("20:00"))
+            ],
+            7 =>
+            [
+                new DurationLessonDetails(TimeOnly.Parse("20:05"), TimeOnly.Parse("20:50")),
+                new DurationLessonDetails(TimeOnly.Parse("21:00"), TimeOnly.Parse("21:45"))
+            ],
+
+            _ => []
+        };
+
+        var startIndex = scheduleItem.Number > 0 ? scheduleItem.Number - 1 : 0;
+        var endIndex = scheduleItem.Number > 0 ? startIndex : scheduleCalls.Count - 1;
+        return scheduleCalls[startIndex..endIndex];
     }
 }
