f7b7375cbefa811e49bea8f6c3268a4b214b35b2
maksim789456 <git@capybaraesttravy.ru>
2024-12-22 15:31:43 +0400

    Rewrite examples

--- ClientSamgkExample/Program.cs
+++ ClientSamgkExample/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ClientSamgk;
+using ClientSamgk.Controllers;
 using ClientSamgk.Interfaces.Client;
+using ClientSamgk.Models;
 using ClientSamgkOutputResponse.Enums;
 
 IClientSamgkApi api = new ClientSamgkApi();
@@ -13,7 +15,10 @@ var groupsTeachers = await api.Accounts.GetTeachersAsync();
 
 // Получение расписание за день
 DateOnly dateOnly = new DateOnly(2024,09,16);
-var scheduleFromDate = await api.Schedule.GetScheduleAsync(dateOnly, ScheduleSearchType.Employee, "2294");
+var query = new ScheduleQuery()
+    .WithDate(dateOnly)
+    .WithSearchType(ScheduleSearchType.Employee, "2294");
+var scheduleFromDate = await api.Schedule.GetScheduleAsync(query);
 
 // или с использованием объектов реализующих интерфейсы
 // IOutResultIdentity, IOutResultCab, IOutResultGroup
@@ -21,23 +26,33 @@ var obj = await api.Groups.GetGroupAsync("ис-23-01");
 
 if (obj is null) throw new Exception($"{nameof(obj)} is null)");
 
-scheduleFromDate = await api.Schedule.GetScheduleAsync(dateOnly, obj);
+query = new ScheduleQuery()
+    .WithDate(dateOnly)
+    .WithGroup(obj);
+scheduleFromDate = await api.Schedule.GetScheduleAsync(query);
     
 // Получение расписание диапозона дат
 DateOnly dateOnlyStart = new DateOnly(2024,09,16);
 DateOnly dateOnlyEnd = new DateOnly(2024,09,16);
 
+query = new ScheduleQuery()
+    .WithDateRange(dateOnlyStart, dateOnlyEnd)
+    .WithGroup(obj);
 var resultScheduleCollection = await api.Schedule
-    .GetScheduleAsync(dateOnlyStart, dateOnlyEnd, obj);
+    .GetScheduleAsync(query);
     
 // Получение расписание за день по преподавателям, кабинету или группам
 // долгая выгрузка
+query = new ScheduleQuery()
+    .WithDate(dateOnlyStart)
+    .WithAllForSearchType(ScheduleSearchType.Employee)
+    .WithDelay(1000);
 var resultScheduleCollectionFromDateAll = await api.Schedule
-    .GetAllScheduleAsync(dateOnlyStart, ScheduleSearchType.Employee, delay: 1000);
+    .GetScheduleAsync(query);
     
 // пример вывода расписания
 
-foreach (var item in scheduleFromDate.Lessons)
+foreach (var item in scheduleFromDate.FirstOrDefault().Lessons)
 {
     Console.WriteLine($"{item.NumPair}.{item.NumLesson} - {item.SubjectDetails.FullSubjectName}");
 }
