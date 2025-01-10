a4b4efe1d5b4f8f607460ac2f1ee414d939be2f4
kulagin <alex_kulagin@samgk.ru>
2025-01-10 13:58:48 +0400

    Модель для передачи настроек кеша

---
+++ ClientSamgk.Models.Params/Implementations/Cache/CacheOptions.cs
@@ -0,0 +1,23 @@
+﻿using ClientSamgk.Models.Params.Interfaces.Cache;
+
+namespace ClientSamgk.Models.Params.Implementations.Cache;
+
+public class CacheOptions : ICacheOptions
+{
+    public CacheOptions()
+    {
+        
+    }
+
+    public CacheOptions(int lifeTimeInMinutesForCommon, int 
+        lifeTimeInMinutesLong, int lifeTimeInMinutesShort)
+    {
+        LifeTimeCommonObjects = lifeTimeInMinutesForCommon;
+        LifeTimeObjectsForLong = lifeTimeInMinutesLong;
+        LifeTimeObjectsForShort = lifeTimeInMinutesShort;
+    }
+
+    public int LifeTimeCommonObjects { get; }
+    public int LifeTimeObjectsForLong { get; }
+    public int LifeTimeObjectsForShort { get; }
+}
---
+++ ClientSamgk.Models.Params/Interfaces/Cache/ICacheOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace ClientSamgk.Models.Params.Interfaces.Cache;
+
+public interface ICacheOptions
+{
+    int LifeTimeCommonObjects { get; }
+    int LifeTimeObjectsForLong { get;  }
+    int LifeTimeObjectsForShort { get;  }
+}
