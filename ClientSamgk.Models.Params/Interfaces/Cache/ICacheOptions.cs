2a23d55e717fc2ddbae931b314c15263528a1828
kulagin <alex_kulagin@samgk.ru>
2025-01-10 14:03:32 +0400

    Комментарии

--- ClientSamgk.Models.Params/Interfaces/Cache/ICacheOptions.cs
+++ ClientSamgk.Models.Params/Interfaces/Cache/ICacheOptions.cs
@@ -2,7 +2,19 @@
 
 public interface ICacheOptions
 {
+    /// <summary>
+    /// Срок жизни общих объектов (списки преподавателей, групп, кабинетов.
+    /// в минутах
+    /// </summary>
     int LifeTimeCommonObjects { get; }
+    /// <summary>
+    /// Срок жизни долгих и не меняющихся объектов (например прошедшее расписание)
+    /// в минутах
+    /// </summary>
     int LifeTimeObjectsForLong { get;  }
+    /// <summary>
+    /// Срок жизни часто меняющихся объектов (например, расписание на сегодня и последующие дни)
+    /// в минутах
+    /// </summary>
     int LifeTimeObjectsForShort { get;  }
 }
