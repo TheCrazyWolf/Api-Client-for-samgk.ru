2b43e55a58d886b990a3f2ee708a60e7013dbfad
TheCrazyWolf <74318176+TheCrazyWolf@users.noreply.github.com>
2025-11-06 21:25:16 +0400

    Merge pull request #43
    * Новые звонки
    
    * Обновил звонки
    
    * версия

--- ClientSamgk.Models.Enums/Schedule/ScheduleCallType.cs
+++ ClientSamgk.Models.Enums/Schedule/ScheduleCallType.cs
@@ -4,9 +4,9 @@ namespace ClientSamgk.Models.Enums.Schedule;
 
 public enum ScheduleCallType
 {
-    [Display(Name = "Обычное")]
+    [Display(Name = "Обычное (до 05.11.25)")]
     Standart,
-    [Display(Name = "Обычное сокращенное")]
+    [Display(Name = "Обычное сокращенное (до 05.11.25)")]
     StandartShort,
     [Display(Name = "Сокращенное (занятие менее 1ч.)")]
     SuperShort,
@@ -16,4 +16,8 @@ public enum ScheduleCallType
     SuperShortWithShift,
     [Display(Name = "Сокращенное со сдвигом")]
     ShortWithShift,
+    [Display(Name = "ОБЫЧНОЕ_С_05.11.25")]
+    StandartWith05112025,
+    [Display(Name = "ОБЫЧНОЕ_СОКР_ЗАН_1ЧАС_БЕЗ_СДВИГА_С_05.11.25")]
+    StandartShortWith05112025,
 }
